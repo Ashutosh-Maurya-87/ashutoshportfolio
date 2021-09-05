c9c31384d66475b05671b9b2a7633f1e69d0ef55
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-09-05 12:48:29 +0530

    'fix-some-issue'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
-import Info from './components/Info';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Info from "./components/Info";
+import Navbar from "./components/Navbar";
+import background from "./images/backgroundImage.jpg";
 
 function App() {
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "100%",
+        width: "100%",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <Navbar />
       <Info />
       <h2>this is a portfolio</h2>
