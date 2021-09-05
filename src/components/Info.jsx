5424ed0b2687f6dd78751e4b8574ffbbb72ab218
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-09-05 12:52:27 +0530

    'update'

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import './Info.css';
+import "./Info.css";
 
-const Info = () =>{
-    return (
-        <>
-        <div className='info'>
+const Info = () => {
+  return (
+    <>
+      <div className="info">
         <h3>this is info component</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />Minima in facere quidem ullam veritatis,<br /> ut enim at iusto voluptatem recusandae id laboriosam numquam blanditiis itaque <br /> facilis incidunt hic iste possimus.</p>
-        </div>
-        </>
-    )
-}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <br /> Minima in facere quidem ullam veritatis,
+          <br /> ut enim at iusto voluptatem recusandae id laboriosam numquam
+          blanditiis itaque
+          <br /> facilis incidunt hic iste possimus.
+        </p>
+      </div>
+    </>
+  );
+};
 export default Info;
