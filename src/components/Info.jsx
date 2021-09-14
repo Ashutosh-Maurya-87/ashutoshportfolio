c0f91aee069bdc405cee0569a697957d4dce6481
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-09-14 19:36:54 +0530

    'some-change'

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,11 +5,27 @@ const Info = () => {
   return (
     <>
       <div className="info">
-        <h3>this is info component</h3>
-        <div
-          className="card"
-          style={{ width: "18rem", backgroundColor: "red" }}
-        >
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            Go somewhere
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            Go somewhere
+          </div>
+        </div>
+        <div className="card">
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
