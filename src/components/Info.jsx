d565b7a08de275e630ad5be708fbe0f10fe4234b
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-09-20 10:27:50 +0530

    'fix-and-update-more'

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,35 +7,26 @@ const Info = () => {
       <div className="info">
         <div className="card">
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">About:-</h5>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              <h4>
+                <br />
+                Myself Ashutosh Maurya. <br />
+                I have Completed BCA degree From Dr. Ram Manohar Lohia Awadh
+                University Ayodhya Uttar Pradesh.
+                <br />
+              </h4>
             </p>
-            Go somewhere
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            Go somewhere
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            Go somewhere
           </div>
         </div>
       </div>
+      <h2>
+        Education:-
+        <li>12th</li>
+        <li>12th</li>
+        <li>12th</li>
+        <li>12th</li>
+      </h2>
     </>
   );
 };
