d1cf949fbe8ba22d72ff1a0ebb028193551a8122
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-09-20 11:44:18 +0530

    'fix-and-update-more'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import './Navbar.css'
-const Navbar = () =>{
-    return (
-        <>
-        <div className='nav-bar'>
-            <header>
-                <ul className="list-item">
-                    <li>Home</li>
-                    <li>Contact</li>
-                    <li>More</li>
-        
-                </ul>
-            </header>
-        </div>
-        
-        </>
-    )
-}
+import React from "react";
+import "./Navbar.css";
+import img from "../images/portfolioimg.png";
+const Navbar = () => {
+  return (
+    <>
+      <div className="nav-bar">
+        <header>
+          <ul className="list-item">
+            <li className="nav-image">
+              <img src={img} alt="img" />
+            </li>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>More</li>
+          </ul>
+        </header>
+      </div>
+    </>
+  );
+};
 
-export default Navbar ;
+export default Navbar;
